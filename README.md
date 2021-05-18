9cfac12c9b846fc0f8c4c79611f240cf0582c696
zhangrl <287037213@qq.com>
2021-05-18 09:26:42 +0800

    blog项目构建

---
+++ README.md
@@ -0,0 +1,19 @@
+项目结构
+
+##action        action模块
+
+##config        配置模块
+
+##controller    模块入口模块
+
+##model         实体类模块
+
+##public        工具模块
+
+##service       状态修改文件
+
+##style         样式模块
+
+##util          工具模块
+
+##view          ui界面模块
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+import { Button,message } from 'antd'
+
+import './style/App.css'
+
+class App extends Component{
+
+    render(){
+        return (
+            <Button type="primary" onClick={() => message.success("点击成功")}>App</Button>
+        )
+    }
+}
+
+export default App
